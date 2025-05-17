a6e2cad65be863e89ddae48211e65d4e44e3d286
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-05-17 21:30:22 +0300

    Adding investment result component, a table in it, show results in it and style it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,25 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import { InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Investment Calculator';
+  resultData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
 
   onCalculateInvestmentResults(data: InvestmentInput) {
     const { initialInvestment, annualInvestment, duration, expectedReturn } =
@@ -35,6 +44,6 @@ export class AppComponent {
       });
     }
 
-    console.log(annualData);
+    this.resultData = annualData;
   }
 }
