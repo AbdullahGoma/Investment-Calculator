cfc13333b841e3e89d85f7ca6d146cba3b5669dd
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-05-17 21:55:11 +0300

    Using Signals

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input } from '@angular/core';
-import { InvestmentInput } from '../investment-input.model';
+import { Component, input } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
 
 @Component({
@@ -7,15 +6,17 @@ import { CurrencyPipe } from '@angular/common';
   standalone: true,
   imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
-  styleUrl: './investment-results.component.css'
+  styleUrl: './investment-results.component.css',
 })
 export class InvestmentResultsComponent {
-  @Input() results?:{
-    year: number,
-    interest: number,
-    valueEndOfYear: number,
-    annualInvestment: number,
-    totalInterest: number,
-    totalAmountInvested: number
-  }[];
+  results = input<
+    {
+      year: number;
+      interest: number;
+      valueEndOfYear: number;
+      annualInvestment: number;
+      totalInterest: number;
+      totalAmountInvested: number;
+    }[]
+  >();
 }
